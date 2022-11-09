2e15648266645cfcdc2b4e6633252cb6bc1f846f
chisela14 <chisela14@gmail.com>
2022-11-09 18:23:38 +0100

    css login

--- src/main/java/com/jacaranda/ddbb/UserControl.java
+++ src/main/java/com/jacaranda/ddbb/UserControl.java
@@ -1,19 +1,16 @@
 package com.jacaranda.ddbb;
 
 import com.jacaranda.model.User;
-
-import javax.persistence.Query;
-
 import org.hibernate.Session;
 
 public class UserControl {
 	
 	public static User getUser(String username) throws ControlException {
 		Session session = ConnectionDDBB.getSession();
-		// User result = (User)session.get(User.class, username);
-		Query query = session.createQuery("FROM com.jacaranda.model.User WHERE username= :username"); //mirar sintaxis
-		query.setParameter("username", username);
-		User result = (User) query.getSingleResult();
+		User result = (User)session.get(User.class, username);
+//		Query query = session.createQuery("FROM com.jacaranda.model.User WHERE username= :username"); //mirar sintaxis
+//		query.setParameter("username", username);
+//		User result = (User) query.getSingleResult();
 		if(result == null) {
 			throw new ControlException("No se ha encontrado el usuario en la base de datos");
 		}
